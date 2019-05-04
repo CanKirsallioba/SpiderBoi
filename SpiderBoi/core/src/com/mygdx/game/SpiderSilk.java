2de958b571a9fa7da202439c626439c9ff0c7826
mrOdinson <cankirsallioba@gmail.com>
2019-05-04 15:53:23 +0300

    Code version 04.05.2019 Code by cnkrmc

---
+++ SpiderBoi/core/src/com/mygdx/game/SpiderSilk.java
@@ -0,0 +1,27 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.ArrayList;
+
+public class SpiderSilk {
+
+    //properties
+    private ArrayList<Vector2> silkCoords;
+    private boolean isKnot;
+
+    //methods
+
+    public ArrayList<Vector2> getSilkCoords() {
+        return silkCoords;
+    }
+
+    public boolean isKnot() {
+        return isKnot;
+    }
+
+    public void draawSilk()
+    {
+
+    }
+}
---
+++ SpiderBoi/core/src/com/mygdx/game/SpikyObstacle.java
@@ -0,0 +1,15 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+public class SpikyObstacle extends Obstacle {
+
+    //constructor
+    public SpikyObstacle(int x, int y)
+    {
+        super();
+        position = new Vector2(x, y);
+        image = new Texture("SpikyObstacle.png");
+    }
+}
