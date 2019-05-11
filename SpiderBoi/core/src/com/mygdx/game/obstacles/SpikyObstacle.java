fe7612474b07c98474aa38669109153697df8b09
burakyuslu <48166202+burakyuslu@users.noreply.github.com>
2019-05-11 16:37:38 +0300

    Update SpikyObstacle.java
    Tweaked Can's checkCollision() method for Obstacle class a bit to make it function for SpikyObstacle.

--- SpiderBoi/core/src/com/mygdx/game/obstacles/SpikyObstacle.java
+++ SpiderBoi/core/src/com/mygdx/game/obstacles/SpikyObstacle.java
@@ -3,7 +3,7 @@ package com.mygdx.game.obstacles;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 
-public class SpikyObstacle extends Obstacle {
+public class SpikyObstacle extends Obstacle{
 
     //constructor
     /**
@@ -16,5 +16,36 @@ public class SpikyObstacle extends Obstacle {
         super();
         position = new Vector2(x, y);
         image = new Texture("SpikyObstacle.png");
+    }
+
+    /**
+     * Checks for collisions between SpiderBoi and the SpikyObstacle, removes SpiderBoi if there is one.
+     * @param spiderBoi is the main character it will be checked if it collides with the SpikyObstacle.
+     * @return a boolean value if it collides returns true if not returns false.
+     */
+    @Override
+    public boolean checkCollision(SpiderBoi spiderBoi)
+    {
+        if(spiderBoi.getBoundary().overlaps(getBoundary()))
+        {
+            Vector2 reverseVel = spiderBoi.getVelocity().scl(-1);
+            while(spiderBoi.getBoundary().overlaps(getBoundary())) {
+                spiderBoi.setVelocity(reverseVel);
+                spiderBoi.move();
+            }
+            spiderBoi.getVelocity().setZero();
+            spiderBoi.setOnObstacle(true);
+        }
+
+        if (spiderBoi.getPosition().x + spiderBoi.getImage().getWidth() / 2 > Gdx.graphics.getWidth()
+                || spiderBoi.getPosition().x < 0
+                || spiderBoi.getPosition().y + spiderBoi.getImage().getHeight() / 2 > Gdx.graphics.getHeight()
+                || spiderBoi.getPosition().y < 0)
+        {
+            spiderBoi.getVelocity().setZero(); // Stop the SpiderBoi
+            spiderBoi.setPresent( false); // rip
+            // Open the "You lost the level" GUI screen.
+        }
+        return spiderBoi.isOnObstacle();
     }
 }
