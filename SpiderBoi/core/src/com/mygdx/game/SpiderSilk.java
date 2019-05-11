1b9f34ee3e29acfe6756a71c041ad378b0fd648c
cnkrmc <48177030+cnkrmc@users.noreply.github.com>
2019-05-11 15:18:00 +0300

    Update SpiderSilk.java

--- SpiderBoi/core/src/com/mygdx/game/SpiderSilk.java
+++ SpiderBoi/core/src/com/mygdx/game/SpiderSilk.java
@@ -34,29 +34,42 @@ public class SpiderSilk {
 
 
 
-    public int checkKnot () {
-        int count = 0;
-        for (int i = 0; i < silkList.size() - 1; i++) {
-            for (int j = 0; j < silkList.size() - 1; j++) {
-                if (i != j + 1 || i != j - 1) {
-                    if (silkList.get(i).overlaps(silkList.get(j))) {
-                        count++;
-                    }
-                }
-            }
+     public boolean checkKnot () {
+        if(spiderBoi.getVelocity().isZero()){
+            lastHit = silkList.size()-1;
+            return false;
+
         }
-        return count - 1;
+        Boolean ans = false;
+        for(int i = 0; i< silkList.size();i++)
+        {
+            ans = (i != lastHit) && (ans || spiderBoi.getBoundary().overlaps(silkList.get(i)));
+            if(spiderBoi.getBoundary().overlaps(silkList.get(i)))
+                lastHit = i;
+        }
+        if(lastHit != -1 && !spiderBoi.getBoundary().overlaps(silkList.get(lastHit)))
+            lastHit = -1;
+        return ans;
     }
 
     /**
      * This method draws the silk coming out of the spiderBoi, taking the endpoint as th emiddle of the spiderBoi image
      * it stops drawing when it reaches an obstacle, so when it's velocity is zero.
      */
-    public void drawSilk()
-    {
-        if(spiderBoi.isOnObstacle())
-        {
+   public void drawSilk() {
+        if (spiderBoi.isOnObstacle()) {
             spiderBoi.getStopLocations().add(spiderBoi.getPosition().cpy().add(spiderBoi.getHalfSize()));
+            if (spiderBoi.getStopLocations().size() > 1) {
+                Vector2 lastLoc = spiderBoi.getStopLocations().get(spiderBoi.getStopLocations().size() - 1);
+                Vector2 lastLoc2 = spiderBoi.getStopLocations().get(spiderBoi.getStopLocations().size() - 2);
+                if (lastLoc.y == lastLoc2.y)
+                    silkList.add(new Rectangle(min(lastLoc.x, lastLoc2.x)-5, lastLoc.y-5,
+                            Math.abs(lastLoc.x - lastLoc2.x)+10, 10));
+
+                else if (lastLoc.x == lastLoc2.x)
+                    silkList.add(new Rectangle(lastLoc.x-5, min(lastLoc.y, lastLoc2.y)-5,
+                            10, Math.abs(lastLoc.y - lastLoc2.y)));
+            }
             spiderBoi.setOnObstacle(false);
         }
 
@@ -64,22 +77,25 @@ public class SpiderSilk {
         shapeRenderer.setColor(1, 1, 1, 1);
 
 
-        if(spiderBoi.getStopLocations().size() > 1) {
-            for (int i = 0; i < spiderBoi.getStopLocations().size() - 1; i++) {
-                shapeRenderer.rectLine(spiderBoi.getStopLocations().get(i), spiderBoi.getStopLocations().get(i + 1), 10);
-
-                if (spiderBoi.getStopLocations().get(i).y == spiderBoi.getStopLocations().get(i + 1).y)
-                    silkList.add(new Rectangle(spiderBoi.getStopLocations().get(i).x, spiderBoi.getStopLocations().get(i).y,
-                            Math.abs(spiderBoi.getStopLocations().get(i + 1).x - spiderBoi.getStopLocations().get(i).x), 10));
-
-                else if (spiderBoi.getStopLocations().get(i).x == spiderBoi.getStopLocations().get(i + 1).x) {
-                    silkList.add(new Rectangle(spiderBoi.getStopLocations().get(i).x, spiderBoi.getStopLocations().get(i).y,
-                            10, Math.abs(spiderBoi.getStopLocations().get(i + 1).y - spiderBoi.getStopLocations().get(i).y)));
-                }
-            }
+        for (int i = 0; i < silkList.size(); i++) {
+            //shapeRenderer.rectLine(spiderBoi.getStopLocations().get(i), spiderBoi.getStopLocations().get(i + 1), 10);
+            Rectangle tmp = silkList.get(i);
+            shapeRenderer.rect(tmp.getX(), tmp.getY(), tmp.getWidth(), tmp.getHeight());
         }
 
 
+
+        /*
+        for (int i = 0; i < silkList.size() -1; i++) {
+            for (int j = 0; j < silkList.size() -1; j++) {
+                 if (i == j) {                                 TO BE FIXED!!! THIS IMPLEMENTATION IS WRONG
+                     j++;
+                     silkList.get(i)
+
+                 }
+            }
+        }
+        */
 
         shapeRenderer.rectLine(spiderBoi.getStopLocations().get(spiderBoi.getStopLocations().size() - 1),
                 spiderBoi.getPosition().cpy().add(spiderBoi.getHalfSize()), 10);
