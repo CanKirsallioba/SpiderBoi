09c698d31a31034c3d7ffe132bbf2ad7b1c288d2
brstftk123 <48119937+brstftk123@users.noreply.github.com>
2019-05-14 19:28:50 +0300

    Update MainMenu.java

--- SpiderBoi/core/src/com/mygdx/game/GUI/MainMenu.java
+++ SpiderBoi/core/src/com/mygdx/game/GUI/MainMenu.java
@@ -6,6 +6,9 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector;
 import com.badlogic.gdx.math.Vector2;
 
+/**
+ * A main menu class before starting the actual Spider Boi Game
+ */
 public class MainMenu {
 
     //properties
@@ -13,6 +16,9 @@ public class MainMenu {
     Texture title;
 
     //constructor
+    /**
+     * A constructor for the main menu class
+     */
     public MainMenu()
     {
         playButton = new Button("playBut1.png", "playBut2.png", 0, 680) ;
@@ -29,6 +35,10 @@ public class MainMenu {
     }
 
     //methods
+    /**
+     * A method that draws the main menu
+     * @param batch a SpriteBatch object that helps drawing
+     */
     public void draw(SpriteBatch batch)
     {
         batch.begin();
@@ -41,6 +51,9 @@ public class MainMenu {
         batch.end();
     }
 
+    /**
+     * A reset method for the main menu buttons
+     */
     public void resetAll()
     {
         playButton.reset();
@@ -50,6 +63,11 @@ public class MainMenu {
         exitButton.reset();
     }
 
+    /**
+     * A method that finds and darkens the pressed button among others
+     * @param x the x location of the press
+     * @param y the y location of the press
+     */
     public void touchDown(int x, int y)
     {
         Vector2 lastTouchInv = new Vector2(x, Gdx.graphics.getHeight() - y );
@@ -65,26 +83,51 @@ public class MainMenu {
             exitButton.setDark();
     }
 
+    /**
+     * A method that checks if play button is pressed
+     * @param pos the position of the press
+     * @return playButton.isPressed(pos)
+     */
     public boolean playButtonPressed(Vector2 pos)
     {
         return playButton.isPressed(pos);
     }
 
+    /**
+     * A method that checks if store button is pressed
+     * @param pos the position of the press
+     * @return storeButton.isPressed(pos)
+     */
     public boolean storeButtonPressed(Vector2 pos)
     {
         return storeButton.isPressed(pos);
     }
 
+    /**
+     * A method that checks if achievements button is pressed
+     * @param pos the position of the press
+     * @return achievementsButton.isPressed(pos)
+     */
     public boolean achievementsButtonPressed(Vector2 pos)
     {
         return achievementsButton.isPressed(pos);
     }
 
+     /**
+     * A method that checks if about button is pressed
+     * @param pos the position of the press
+     * @return aboutButton.isPressed(pos)
+     */
     public boolean aboutButtonPressed(Vector2 pos)
     {
         return aboutButton.isPressed(pos);
     }
 
+    /**
+     * A method that checks if exit button is pressed
+     * @param pos the position of the press
+     * @return exitButton.isPressed(pos)
+     */
     public boolean exitButtonPressed(Vector2 pos)
     {
         return exitButton.isPressed(pos);
